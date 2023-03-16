daaddb25c72760cd3e7b81dcb122561ca85c1650
JoelCupeles <joel.cupeles1998@gmail.com>
2023-03-16 10:48:23 -0500

    HTML changes and pseudocode in JS

---
+++ index.js
@@ -0,0 +1,25 @@
+// Initialize variables and elements
+// - apiKey
+// - searchForm
+// - cityInput
+// - searchHistory
+// - currentWeather
+// - forecast
+
+// Function displayWeather(data)
+// - Display city name, date, weather icon, temperature, humidity, and wind speed in currentWeather element
+
+// Function displayForecast(data)
+// - Iterate through forecast data
+// - For each day, create a forecast item with date, weather icon, temperature, wind speed, and humidity
+// - Append forecast item to forecast element
+
+// Function fetchWeather(city)
+// - Fetch current weather data for the city using the API
+
+// - Add click event listener to fetch weather data when the history item is clicked
+
+
+// Event listener for searchForm submit event
+
+// - Fetch and display weather data for the city using fetchWeather function
