de3fcaade278660b8fec366b60f97757ccd912d8
JoelCupeles <joel.cupeles1998@gmail.com>
2023-03-19 14:08:03 -0500

    JS fuction working, data displaying

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
-// Initialize variables and elements
-// - apiKey
-// - searchForm
-// - cityInput
-// - searchHistory
-// - currentWeather
-// - forecast
+// script.js
+const searchForm = document.getElementById("search-form");
+const cityInput = document.getElementById("city-input");
+const searchHistory = document.getElementById("search-history");
+const currentWeather = document.getElementById("current-weather");
+const forecast = document.getElementById("forecast");
 
-// Function displayWeather(data)
-// - Display city name, date, weather icon, temperature, humidity, and wind speed in currentWeather element
-
-// Function displayForecast(data)
-// - Iterate through forecast data
-// - For each day, create a forecast item with date, weather icon, temperature, wind speed, and humidity
-// - Append forecast item to forecast element
-
-// Function fetchWeather(city)
-// - Fetch current weather data for the city using the API
-
-// - Add click event listener to fetch weather data when the history item is clicked
-
-
-// Event listener for searchForm submit event
-
-// - Fetch and display weather data for the city using fetchWeather function
+function displayWeather(data) {
+    currentWeather.innerHTML = `
+      <h2>${data.name} (${new Date().toLocaleDateString()})</h2>
+      <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}">
+      <p>Temperature: ${data.main.temp}°F</p>
+      <p>Humidity: ${data.main.humidity}%</p>
+      <p>Wind Speed: ${data.wind.speed} MPH</p>
+    `;
+  }
+  
+  function displayForecast(data) {
+    forecast.innerHTML = "";
+  
+    data.list.forEach((item, index) => {
+      if (index % 8 === 0) {
+        const forecastItem = document.createElement("div");
+        forecastItem.classList.add("forecast-item");
+  
+        forecastItem.innerHTML = `
+          <h3>${new Date(item.dt * 1000).toLocaleDateString()}</h3>
+          <img src="https://openweathermap.org/img/wn/${item.weather[0].icon}.png" alt="${item.weather[0].description}">
+          <p>Temp: ${item.main.temp}°F</p>
+          <p>Wind: ${item.wind.speed} MPH</p>
+          <p>Humidity: ${item.main.humidity}%</p>
+        `;
+  
+        forecast.appendChild(forecastItem);
+      }
+    });
+  }
+  
+  async function fetchWeather(city) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f461f184bb0f987cdbcdefa61ead2cc9&units=imperial`);
+    const data = await response.json();
+    displayWeather(data);
+    const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=f461f184bb0f987cdbcdefa61ead2cc9&units=imperial`);
+    const forecastData = await forecastResponse.json();
+    displayForecast(forecastData);
+  }
+  
+  function addToHistory(city) {
+    const listItem = document.createElement("div");
+    listItem.classList.add("history-item");
+    listItem.textContent = city;
+    listItem.addEventListener("click", () => fetchWeather(city));
+    searchHistory.appendChild(listItem);
+  }
+  
+  searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const city = cityInput.value.trim();
+    if (city) {
+      fetchWeather(city);
+      addToHistory(city);
+      cityInput.value = "";
+    }
+  });
